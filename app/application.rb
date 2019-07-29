b5651201038f69d52e9e2de170488b4c6f76e9ef
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-07-29 09:51:49 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,7 +15,7 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      @@cart.each do |cart|
+      if @@cart.each do |cart|
         resp.write "#{cart}\n"
       end 
     elsif
